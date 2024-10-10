d52c3f5be431e77bc8e199387e64263a092513a1
Yudha Ananda Kresna <yakresna@pvg.co.id>
2024-10-10 20:27:17 +0700

    feat: change code structure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import torch.nn as nn
 import numpy as np
-from torch.utils.data import Dataset, DataLoader
 from transformers import PreTrainedModel, PretrainedConfig
 import torch
 import json
@@ -10,17 +9,19 @@ import gdown
 
 app = Flask(__name__)
 
-# Load the model and tokenizer (assum
 
 @app.route('/generate', methods=['POST'])
 def generate():
     data = request.json
     seed_text = data['seed_text']
 
-    # Call the generate_code function with the seed text and length
-    generated_code = generate_code(seed_text, 8)  # Function you implemented
+    generated_code = generate_code(seed_text, 8) 
 
     return jsonify({"generated_code": generated_code})
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({"status": "ok"})
 
 def download(file_id, file_name):
     # Create the download URL
@@ -45,63 +46,63 @@ if __name__ == "__main__":
 
     # Hyperparameters
     hidden_size = 128
+    app.run(debug=False)
 
-    class LSTMModelConfig(PretrainedConfig):
-        model_type = "LSTM"
+class LSTMModelConfig(PretrainedConfig):
+    model_type = "LSTM"
 
-        def __init__(self, vocab_size, hidden_size, num_layers=1, **kwargs):
-            super().__init__(**kwargs)
-            self.vocab_size = vocab_size
-            self.hidden_size = hidden_size
-            self.num_layers = num_layers
+    def __init__(self, vocab_size, hidden_size, num_layers=1, **kwargs):
+        super().__init__(**kwargs)
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
 
-    # 3. Model LSTM di PyTorch
-    class LSTMModel(PreTrainedModel):
-        def __init__(self, config:LSTMModelConfig):
-            super(LSTMModel, self).__init__(config)
-            self.embedding = nn.Embedding(config.vocab_size, config.hidden_size)
-            self.lstm = nn.LSTM(config.hidden_size, config.hidden_size, config.num_layers, batch_first=True)
-            self.fc = nn.Linear(config.hidden_size, config.vocab_size)
-        
-        def forward(self, x, hidden):
-            x = self.embedding(x)
-            out, hidden = self.lstm(x, hidden)
-            out = self.fc(out[:, -1, :])  # Mengambil output dari LSTM pada waktu terakhir
-            return out, hidden
+# 3. Model LSTM di PyTorch
+class LSTMModel(PreTrainedModel):
+    def __init__(self, config:LSTMModelConfig):
+        super(LSTMModel, self).__init__(config)
+        self.embedding = nn.Embedding(config.vocab_size, config.hidden_size)
+        self.lstm = nn.LSTM(config.hidden_size, config.hidden_size, config.num_layers, batch_first=True)
+        self.fc = nn.Linear(config.hidden_size, config.vocab_size)
+    
+    def forward(self, x, hidden):
+        x = self.embedding(x)
+        out, hidden = self.lstm(x, hidden)
+        out = self.fc(out[:, -1, :])  # Mengambil output dari LSTM pada waktu terakhir
+        return out, hidden
 
-        def init_hidden(self, batch_size):
-            # Inisialisasi hidden state dan cell state dengan nol
-            return (torch.zeros(1, batch_size, config.hidden_size).to(device),
-                    torch.zeros(1, batch_size, config.hidden_size).to(device))
-        
-        def save_pretrained(self, save_directory):
-            # Save model parameters
-            torch.save(self.state_dict(), f"{save_directory}/pytorch_model.bin")
+    def init_hidden(self, batch_size):
+        # Inisialisasi hidden state dan cell state dengan nol
+        return (torch.zeros(1, batch_size, config.hidden_size).to(device),
+                torch.zeros(1, batch_size, config.hidden_size).to(device))
+    
+    def save_pretrained(self, save_directory):
+        # Save model parameters
+        torch.save(self.state_dict(), f"{save_directory}/pytorch_model.bin")
 
-    # 4. Melatih Model
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# 4. Melatih Model
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    config= LSTMModelConfig(vocab_size, hidden_size)
-    model = LSTMModel(config=config).to(device)
-    model.load_state_dict(torch.load("./pytorch_model.bin", map_location=torch.device(device)))
+config= LSTMModelConfig(vocab_size, hidden_size)
+model = LSTMModel(config=config).to(device)
+model.load_state_dict(torch.load("./pytorch_model.bin", map_location=torch.device(device)))
 
-    def generate_code(seed_text, length):
-        model.eval()
-        generated = seed_text
-        input_seq =  [word_to_idx[word] for word in seed_text.split()]
-        input_seq = torch.tensor(input_seq, dtype=torch.long).unsqueeze(0).to(device)
-        
-        hidden = model.init_hidden(1)
-        
-        with torch.no_grad():
-            for _ in range(length):
-                output, hidden = model(input_seq, hidden)
-                predicted_word_idx = torch.argmax(output).item()
-                predicted_word = idx_to_word[predicted_word_idx]
-                generated += ' '+predicted_word
-                
-                # Update input sequence with the predicted character
-                input_seq = torch.cat([input_seq[:, 1:], torch.tensor([[predicted_word_idx]], dtype=torch.long).to(device)], dim=1)
-        
-        return generated
-    app.run(debug=True)
+def generate_code(seed_text, length):
+    model.eval()
+    generated = seed_text
+    input_seq =  [word_to_idx[word] for word in seed_text.split()]
+    input_seq = torch.tensor(input_seq, dtype=torch.long).unsqueeze(0).to(device)
+    
+    hidden = model.init_hidden(1)
+    
+    with torch.no_grad():
+        for _ in range(length):
+            output, hidden = model(input_seq, hidden)
+            predicted_word_idx = torch.argmax(output).item()
+            predicted_word = idx_to_word[predicted_word_idx]
+            generated += ' '+predicted_word
+            
+            # Update input sequence with the predicted character
+            input_seq = torch.cat([input_seq[:, 1:], torch.tensor([[predicted_word_idx]], dtype=torch.long).to(device)], dim=1)
+    
+    return generated
